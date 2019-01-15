89ddaf1667f07a443d9b5faebde9506326289d0e
vahehambardzumyan <vahe.hambardzumayan@yahoo.com>
2019-01-16 00:19:04 +0400

    for

--- src/year.java
+++ src/year.java
@@ -5,61 +5,13 @@ import java.util.Scanner;
 public class year {
 
     public static void main(String[] args) {
-        System.out.println("Please enter year");
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        System.out.println("Please enter month");
-        int b = scanner.nextInt();
-
-        int monthString;
-        int v;
-        if (a % 4 == 0 && b == 2) {
-            v = 29;
-        } else v=28;
-        switch (b) {
-            case 1:
-                monthString = 31;
-                break;
-            case 2:
-                monthString = v;
-                break;
-            case 3:
-                monthString = 31;
-                break;
-            case 4:
-                monthString = 30;
-                break;
-            case 5:
-                monthString = 31;
-                break;
-            case 6:
-                monthString = 30;
-                break;
-            case 7:
-                monthString = 31;
-                break;
-            case 8:
-                monthString = 31;
-                break;
-            case 9:
-                monthString = 30;
-                break;
-            case 10:
-                monthString = 31;
-                break;
-            case 11:
-                monthString = 30;
-                break;
-            case 12:
-                monthString = 31;
-                break;
-            default:
-                monthString = 0;
-                break;
+        int[] a = {1, 5, 8, 9};
+        int b=0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > b) b = a[i];
         }
+        System.out.println(b);
 
-        System.out.println(monthString);
-
-        }
     }
+}
 
